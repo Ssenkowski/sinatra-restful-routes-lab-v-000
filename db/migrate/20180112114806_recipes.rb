789c81116e62ea9fc21ae0c3478b3190420dd938
Steven Senkowski <scsenkowski@gmail.com>
2018-01-12 11:51:09 +0000

    automatically backed up by learn

--- db/migrate/20180112114806_recipes.rb
+++ db/migrate/20180112114806_recipes.rb
@@ -2,7 +2,7 @@ class Recipes < ActiveRecord::Migration[5.1]
   def change
     create_table :recipes do |c|
       c.string :name
-      c.string :ingredients
+      c.text :ingredients
       c.string :cook_time
   end
 end
