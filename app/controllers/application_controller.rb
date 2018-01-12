fbbe2ec58e7022251e46ec5175925d97fa0ca189
Steven Senkowski <scsenkowski@gmail.com>
2018-01-12 12:17:44 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,12 +6,16 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/recipe/new' do
-    erb :"/recipe/new"
+  get '/recipes'
+  get '/recipes/new' do
+    erb :"/recipes/new"
   end
 
-  post '/recipe/new' do
+  post '/recipes/new' do
     recipe = Recipe.new(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
     recipe.save
+    
     redirect to '/recipes'
   end
+
+  get '/recipes/edit'
